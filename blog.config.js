c54c16631990935ac0570a34814da9d2017c65f3
Asch-MJ <97863361+Asch-MJ@users.noreply.github.com>
2024-06-28 14:17:13 +0800

    Update blog.config.js
    部分隐私内容改到notion中配置

--- blog.config.js
+++ blog.config.js
@@ -25,8 +25,8 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Asch', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '大三软工在读学生', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xmj.asch.blog', // 网站地址
+  BIO: process.env.NEXT_PUBLIC_BIO || '学生', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.asch.blog', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Asch, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
